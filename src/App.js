083d546dce17c9d50e73fea0e4fb97d7c1f65017
Pornpat Nikamanon <pornpatn@gmail.com>
2023-04-12 13:03:31 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function callApi() {
+    fetch('https://simple-node-pat.herokuapp.com/details', { method: 'GET' })
+        .then(data => data.json())
+        .then(json => alert(JSON.stringify(json)))
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <button onClick={callApi}>Call API</button>
+        </header>
+    </div>
+  );
+}
+
+export default App;
